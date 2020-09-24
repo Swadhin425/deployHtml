ca4560238412226601aa07df8329c181c147c601
Ranjan Nayak <swadhin.ranjannayak@siemens.com>
2020-09-24 10:40:51 +0530

    adding node backend

---
+++ index.js
@@ -0,0 +1,20 @@
+const path=require('path');
+const http=require('http');
+const fs=require('fs');
+
+
+const server=http.createServer((req,res)=>{
+    fs.readFile(path.join(__dirname,'public','index.html'),(err,content)=>{
+        if(err) throw err;
+        res.writeHead(200,{'Content-Type': 'text/html'});
+        res.end(content)
+       });
+}
+
+)
+
+const port = process.env.PORT || 5000
+
+server.listen(port, () => {
+  console.log(`Server Running at ${port}`)
+});
