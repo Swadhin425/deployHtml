894a3a9d9e01c7361002a0dc88a3c04071f6928b
Ranjan Nayak <swadhin.ranjannayak@siemens.com>
2020-09-24 11:56:38 +0530

    adding node backend

--- index.js
+++ index.js
@@ -2,19 +2,13 @@ const path=require('path');
 const http=require('http');
 const fs=require('fs');
 
+const express= require('express');
+const app= express();
 
-const server=http.createServer((req,res)=>{
-    fs.readFile(path.join(__dirname,'index.html'),(err,content)=>{
-        if(err) throw err;
-        res.writeHead(200,{'Content-Type': 'text/html'});
-        res.end(content)
-       });
-}
-
-)
+app.use(express.static(path.join(__dirname,"public")))
 
 const port = process.env.PORT || 5000
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server Running at ${port}`)
 });
